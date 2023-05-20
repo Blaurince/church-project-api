803c93134e18a28247665beaaaf7f950a5e1672e
Barbara Laurince <blaurince@hotmail.com>
2023-05-20 01:04:35 -0400

    church api

--- functions/src/events.js
+++ functions/src/events.js
@@ -1,16 +1,23 @@
 import { FieldValue } from "firebase-admin/firestore";
 //mport jwt from "jsonwebtoken";
 import { db } from "./dbConnect.js";
-import { resolvePath } from "react-router-dom";
+//import { resolvePath } from "react-router-dom";
 
 
   const collection = db.collection("events");
 
-export async function getEvents(req, res) {
-  const eventsCollection = await db.collection("events").get()
-  const events = eventsCollection.docs.map(doc => ({...doc.data(), id: doc.id}))
-  res.send(events)
-}
+  export async function getEvents(req, res) {
+    try {
+      const eventsCollection = await db.collection("events").limit(4).get();
+      console.log(eventsCollection.size); // Check the size of the retrieved collection
+      const events = eventsCollection.docs.map(doc => ({...doc.data(), id: doc.id}));
+      console.log(events); // Check the retrieved events
+      res.send(events);
+    } catch (error) {
+      console.error(error); // Log any errors that occur
+      res.status(500).send("Internal Server Error");
+    }
+  }
 
 export async function addEvents(req, res) {
 
@@ -51,12 +58,15 @@ export async function getEvent(req, res) {
 
 export async function updateEvent(req, res) {
   try {
-    const id = req.body.id;
-    delete req.body.id;
+   // const id = req.body.id;
+   // delete req.body.id;
     const data = req.body;
-    const event = await db.collection("events").doc(id).update();
+    const eventRef =  db.collection("events").doc(data.id);
+
+      await  eventRef.update(data);
+
     
-    return res.status(500).send({status: "success", msg: "data updated"})
+    return res.status(200).send({status: "success", msg: "data updated"})
  
   } catch (error) {
      return res.status(500).send({status: 'failed',masg: {erro}})
@@ -66,11 +76,12 @@ export async function updateEvent(req, res) {
 
 export async function deleteEvent(req, res){
 try {
-   const id = req.bodyu.id;
-   const Event =  db.collection("events");
-   await Event.doc(id).delete();
+   const id = req.params.id
+   const EventRef =  db.collection("events").doc(id);
+   await EventRef.delete();
+   await getEvents(req, res)
 
-   return res.status(200).send({status: "success", msg: {error}})
+   return res.status(200).send({status: "success", msg: "event deleted"})
 } catch (error) {
     return res.status(500).send({status: 'failed', msg: {error }})
 }
